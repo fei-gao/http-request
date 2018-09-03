b23632e9fecee920f3ee62daf84b18936d55ee8d
Fei Gao <fgao1@ualberta.ca>
2018-09-03 09:35:10 -0600

    step 1 printing in chunks

---
+++ step1.js
@@ -0,0 +1,31 @@
+var https = require('https');
+
+
+function getAndPrintHTMLChunks () {
+
+    var requestOptions = {
+      host: 'sytantris.github.io',
+      path: '/http-examples/step1.html'
+    };
+  
+    /* Add your code here */
+    https.get(requestOptions, function(response){
+
+    response.setEncoding('utf-8');
+
+    //the callback is invoked when a `data` chunk is received
+    response.on('data', function (data) {
+        console.log(`Chunk of data received. ${data} \n`);
+    });
+
+    // the callback is invoked when all of the data has been received
+    // (the `end` of the stream)
+    response.on('end', function() {
+        console.log('Response stream complete.');
+    });
+
+    })
+  
+  };
+
+  getAndPrintHTMLChunks();
