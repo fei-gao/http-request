9cf4d4c360821b4995d7e6029e4c9fbfe648bb9f
Fei Gao <fgao1@ualberta.ca>
2018-09-04 22:57:32 -0600

    add comments

--- http-functions.js
+++ http-functions.js
@@ -1,14 +1,15 @@
 module.exports = function getHTML (options, callback) {
-    /* Your code here */
     const https = require('https');
 
+    // Send the request
     https.get(options, function(response){
         
+        // Set encoding to UTF-8
         response.setEncoding('utf-8');
         
         let chunks = '';
         //the callback is invoked when a `data` chunk is received
-        //concatenate chunk of data to chunks
+        //append chunk of data to chunks
         response.on('data', function (data) {
             chunks += data;
         });
